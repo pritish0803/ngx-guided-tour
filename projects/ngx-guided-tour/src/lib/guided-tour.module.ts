93e8001672c1fdfc7e01985311bfb3a0bf96136a
pritish0803 <pritish9040246119@gmail.com>
2021-11-16 10:07:20 +0530

    2 files modified

--- projects/ngx-guided-tour/src/lib/guided-tour.module.ts
+++ projects/ngx-guided-tour/src/lib/guided-tour.module.ts
@@ -5,17 +5,17 @@ import { CommonModule } from '@angular/common';
 import { WindowRefService } from './windowref.service';
 
 @NgModule({
-  declarations: [GuidedTourComponent],
-  imports: [CommonModule],
-  providers: [WindowRefService],
-  exports: [GuidedTourComponent],
-  entryComponents: [GuidedTourComponent],
+    declarations: [GuidedTourComponent],
+    imports: [CommonModule],
+    providers: [WindowRefService],
+    exports: [GuidedTourComponent],
+    entryComponents: [GuidedTourComponent]
 })
 export class GuidedTourModule {
-  public static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: GuidedTourModule,
-      providers: [ErrorHandler, GuidedTourService],
-    };
-  }
+    public static forRoot(): ModuleWithProviders<GuidedTourModule> {
+        return {
+            ngModule: GuidedTourModule,
+            providers: [ErrorHandler, GuidedTourService]
+        };
+    }
 }
